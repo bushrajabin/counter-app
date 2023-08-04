c2d383035127c900cc0a03e88b634c83d2496405
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-04 20:17:35 +0530

    added javascript in Increament Button

---
+++ script.js
@@ -0,0 +1,27 @@
+let increment = document.getElementById("btn3");
+let decrement = document.getElementById("btn1")
+let counterValue = document.getElementById("Display");
+var count = 0;
+
+counterValue.innerHTML = count;
+function increase() {
+    count++;
+    counterValue.innerHTML = count;
+    counterValue.style.color = "red";
+}
+
+// function decrease() {
+//     count--;
+//     counterValue.innerHTML = count;
+//     counterValue.style.color = "Green";
+// }
+
+
+// increment.addEventListener('click', increase);
+// decrement.addEventListener('click', decrease);
+
+// function reset() {
+//     let Display = 0;
+//     counterValue.textContent = Display;
+//     counterValue.style.color = "white";
+// }
