a96d2c10bb52391feb185742cdd2a6aa9375b8e2
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-05 11:01:17 +0530

    enable and disabled button added on decrease funtion

--- script.js
+++ script.js
@@ -3,22 +3,31 @@ let decrement = document.getElementById("btn1")
 let counterValue = document.getElementById("Display");
 
 
-var count = 1;
+let count = 1;
 function increase() {
     console.log(count)
 
     count = count + 1
     counterValue.innerHTML = count;
     counterValue.style.color = "red";
+
+
+    document.getElementById('btn1').disabled = false;
 }
 
 function decrease() {
-    if (document.getElementById("btn1").value === "") {
-        Button.disabled = 'true';
-
-    } else {
-        Button.disabled = 'false';
+    if (count >= 1) {
+        count -= 1;
+        document.getElementById("Display").innerHTML = count;
     }
+
+    else {
+        document.getElementById("btn1").disabled = true;
+    }
+
+
+
+
 }
 
 
@@ -26,7 +35,8 @@ function decrease() {
 // decrement.addEventListener('click', decrease);
 
 function reset() {
-var a=0;
-document.getElementById("Display").innerHTML=a;
+    var a = 0;
+    document.getElementById("Display").innerHTML = a;
+
 }
 
