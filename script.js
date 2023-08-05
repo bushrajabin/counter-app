a4c6312ab12901faf58a77ce92e8b84efe848b5b
Bushra jabin <bushrabintjabin@gmail.com>
2023-08-05 09:41:51 +0530

    Disabled negative button

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let increment = document.getElementById("btn3");
 let decrement = document.getElementById("btn1")
 let counterValue = document.getElementById("Display");
-var count = 0;
 
-counterValue.innerHTML = count;
+
+var count = 1;
 function increase() {
-    count++;
+    console.log(count)
+
+    count = count + 1
     counterValue.innerHTML = count;
     counterValue.style.color = "red";
 }
 
 function decrease() {
-    count--;
-    counterValue.innerHTML = count;
-    counterValue.style.color = "Green";
+    if (document.getElementById("btn1").value === "") {
+        Button.disabled = 'true';
+
+    } else {
+        Button.disabled = 'false';
+    }
 }
 
 
@@ -21,9 +26,7 @@ increment.addEventListener('click', increase);
 decrement.addEventListener('click', decrease);
 
 function reset() {
-    let Display = 0;
-    counterValue.textContent = Display;
-    counterValue.style.color = "white";
-    // document.getElementById("Display").value = "";
-    // counterValue.style.color = "white";
+var a=0;
+document.getElementById("Display").innerHTML=a;
 }
+
